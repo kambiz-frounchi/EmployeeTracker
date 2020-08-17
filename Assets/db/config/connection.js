616458ff4970467619a7c70082c03ded2438190e
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-08-17 14:36:20 -0400

    latest changes

---
+++ Assets/db/config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require(`mysql`);
+
+const connection = mysql.createConnection ({
+    host : `localhost`,
+    port : 3306,
+    user: `root`,
+    password: `Kambiz82`,
+    database: `employeeDb`
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -2,3 +2,53 @@ const mysql = require(`mysql`);
 const inquirer = require(`inquirer`);
 const consoleTable = require(`console.table`);
 
+const entryPointQuestion = {
+    type: `list`,
+    name: `entryPoint`,
+    message: `what would you like to do?`,
+    choices: [
+        `view all departments`,
+        `view all roles`,
+        `view all employees`
+        `add department`,
+        `add role`,
+        `add employee`,
+        `update employee role`,
+        `update employee manager`,
+        `view employees by manager`,
+        `delete department`,
+        `delete role`,
+        `delete employee`,
+        `view total utilized department budget`,
+    ]
+};
+
+const prompt = () => {
+    inquirer.prompt(entryPointQuestion).then((answers) => {
+        const employeeTrackerSql = new EmployeeTrackerSql();
+        switch (employeeTrackerSql) {
+            
+        }
+        if (answers.entryPoint === `view`) {
+    
+        }
+    });
+    
+}
+
+const connection = mysql.createConnection ({
+    host : `localhost`,
+    port : 3306,
+    user: `root`,
+    password: `Kambiz82`,
+    database: `employeeDb`
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+
+    prompt();
+});
+
+
